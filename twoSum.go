2b65b805aadaa84b6571ffb9c48498e75ad88ade
Hans <dornsback@gmail.com>
2025-01-19 17:21:01 -0300

    Update twoSum.go

--- twoSum.go
+++ twoSum.go
@@ -5,8 +5,30 @@ You can return the answer in any order.
 */
 
 package main
+
 import "fmt"
 
-func twoSum(nums []int, target int) []int { // takes an array of int and an int as parameters, returns an array of ints as results
+func main() {
+	twoSum([]int{2, 7, 15, 19}, 9)
+}
 
+func twoSum(nums []int, target int) []int { // takes an array of int and an int as parameters, returns an array of ints as results
+	fmt.Println("hi")
+	result := []int{}
+	for i := 0; i < len(nums); i++ {
+		if nums[i]+nums[i+1] == target {
+			fmt.Print("found match. The two numbers on the list ")
+			fmt.Print(nums)
+			fmt.Print(" that add up to ")
+			fmt.Print(target)
+			fmt.Print(" are ")
+			fmt.Print([]int{nums[i], nums[i+1]})
+			result = []int{nums[i], nums[i+1]}
+			return result
+		} else {
+			fmt.Println("no sum can be found to match target")
+			return result
+		}
+	}
+	return result
 }
